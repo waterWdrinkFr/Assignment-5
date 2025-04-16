1b1d00caba572ad7fcb14461de6c481bba734365
Justin Su <justin.su348@gmail.com>
2025-04-15 23:09:32 -0400

    added tailwind to genres.jsx, fixed now playing header in features.jsx

--- Assignment 5/src/Components/Genres.jsx
+++ Assignment 5/src/Components/Genres.jsx
@@ -1,22 +1,17 @@
-import { Link, useLocation } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Genres({ genres }) {
-    const location = useLocation();
-
     return (
-        <div className="p-4">
-            <h3 className="text-lg font-bold mb-4">Genres</h3>
-            <div className="flex flex-wrap gap-2">
-                {genres.map((genre) => {
-                    const isActive = location.pathname === `/movies/genre/${genre.id}`;
-
-                    return (
-                        <button key={genre.id} className={`px-4 py-2 rounded-lg ${isActive ? "bg-blue-500 text-white" : 
-                            "bg-gray-200 text-gray-800 hover:bg-gray-300"}`}>
-                            <Link to={`/movies/genre/${genre.id}`} className="no-underline" > {genre.genre} </Link>
-                        </button>
-                    );
-                })}
+        <div className="genres-container">
+            <h3>Genres</h3>
+            <div className="genres-buttons">
+                {genres.map((genre) => (
+                    <button key={genre.id} className="genre-button">
+                        <Link to={`/movies/genre/${genre.id}`} className="genre-link">
+                            {genre.genre}
+                        </Link>
+                    </button>
+                ))}
             </div>
         </div>
     );
