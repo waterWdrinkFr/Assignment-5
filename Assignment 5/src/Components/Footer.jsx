f5a469390b3ba744b19a17658dd6581f2177c726
JustinSu <Justin.su348@gmail.com>
2025-04-16 13:27:07 +0000

    changed colour theme

--- Assignment 5/src/Components/Footer.jsx
+++ Assignment 5/src/Components/Footer.jsx
@@ -1,10 +1,8 @@
 function Footer() {
     return (
         <div className = "sticky bottom-0 w-full h-[7.5%] bg-gradient-to-t from-blue-900 to-transparent flex justify-between items-center p-4 text-sky-500 text-sm">
-            <p className = "mb-[-30px] ml-1">Follow us on social media | Instagram, X, Facebook</p>
-            <p className = "mb-[-30px] mr-1">
-                About Us | TOS | FAQs | Contacts | Privacy Policy | Cookie Policy | © 2025 JStreaming. All rights reserved.
-            </p>
+            <p className = "mb-[-30px] ml-1">Social media | Instagram, X, Discord</p>
+            <p className = "mb-[-30px] mr-1"> About Us | TOS | FAQs | Contacts | Privacy Policy | Cookie Policy | © 2025 JStreaming. All rights reserved.</p>
         </div>
     );
 }
