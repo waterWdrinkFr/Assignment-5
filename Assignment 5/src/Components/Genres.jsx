a7739b95b36c797c675fd43ead6a437c88e1ae86
Justin Su <justin.su348@gmail.com>
2025-04-19 02:01:06 -0400

    finished genre side bar and adjusted spacing in footer

--- Assignment 5/src/Components/Genres.jsx
+++ Assignment 5/src/Components/Genres.jsx
@@ -3,33 +3,41 @@ import axios from "axios";
 
 function Genres() {
     const [genres, setGenres] = useState([]);
+    const [selectedGenre, setSelectedGenre] = useState(28);
 
     useEffect(() => {
         const fetchGenres = async () => {
             try {
                 const response = await axios.get(
-                    `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US`
+                    `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_TMDB_KEY}`
                 );
                 setGenres(
                     response.data.genres.filter((genre) =>
-                        [12, 18, 16, 80, 10751, 14, 36, 27, 9648, 878, 10752, 37].includes(genre.id)
+                        [28, 12, 16, 80, 10751, 14, 36, 27, 9648, 878, 10752, 37].includes(genre.id)
                     )
                 );
             } catch (error) {
                 console.error("Error fetching genres:", error);
             }
         };
-    
+
         fetchGenres();
     }, []);
 
     return (
         <div>
-            <h2 className="text-2xl font-bold mb-127.5">Specific Genres</h2>
-            <ul className="list-disc pl-5">
+            <h2 className="text-2xl font-bold mt-[110px]">Genres</h2>
+            <ul className="list-none pl-0">
                 {genres.map((genre) => (
-                    <li key={genre.id} className="text-lgt text-white">
-                        {genre.name}
+                    <li key={genre.id} className="mb-3">
+                        <button
+                            className={`px-4 py-2 text-xl font-bold cursor-pointer whitespace-nowrap ${
+                                selectedGenre === genre.id ? "underline text-sky-600" : "text-white"
+                            }`}
+                            onClick={() => setSelectedGenre(genre.id)}
+                        >
+                            {genre.name}
+                        </button>
                     </li>
                 ))}
             </ul>
