6375b67505485ab1c60cc99e51965acc33a648a6
JustinSu <justin.su4@student.tdsb.on.ca>
2025-05-14 12:49:50 -0400

    made logo a bit bigger

--- Assignment 5/src/Components/Header.jsx
+++ Assignment 5/src/Components/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
     return (
         <div className="fixed top-0 left-0 w-full h-[120px] bg-gradient-to-b from-black to-transparent z-20">
             <div className="flex items-center h-full">
-                <button className="mt-0.5 ml-5 w-[200px] p-1 rounded-lg text-3xl font-bold text-white bg-blue-900 cursor-pointer"
+                <button className="mt-0.5 ml-5 w-[220px] p-1 rounded-lg text-3xl font-bold text-white bg-blue-900 cursor-pointer"
                     onClick={() => navigate("/")} > JStreaming </button>
-                <input className="mt-0.5 ml-50 w-[400px] h-[30px] rounded-full border-none px-4 text-base outline-none bg-white text-black"
+                <input className="mt-0.5 ml-45 w-[400px] h-[30px] rounded-full border-none px-4 text-base outline-none bg-white text-black"
                     placeholder="Search Title" />
                 <span className="mt-6 ml-5 h-[40px] w-[70px] text-xl font-bold text-sky-600 underline cursor-pointer whitespace-nowrap"
                     onClick={() => navigate("/movies")} > Explore Movies </span>
